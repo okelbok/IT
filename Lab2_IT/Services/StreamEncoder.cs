5234094ec9206d1f7d2c7ce307e8fed6e2415fd9
Vlada Kolbeko <143844506+okelbok@users.noreply.github.com>
2025-04-04 00:46:21 +0300

    add lab2 (2)

---
+++ Lab2_IT/Services/StreamEncoder.cs
@@ -0,0 +1,60 @@
+﻿namespace Lab2_IT.Services
+{
+	public class StreamEncoder
+	{
+		public byte[] initialBits;
+		public byte[] keyBits;
+		public byte[] resultBits;
+
+		public static int REGISTER_LENGTH = 40;
+		public byte[] register = new byte[REGISTER_LENGTH];
+
+		private static int[] polynomialDegrees = { 40, 21, 19, 2 };
+
+		private byte GetFirstBit(byte[] key)
+		{
+			byte firstBit = 0;
+
+			for (int i = 0; i < polynomialDegrees.Length; i++)
+			{
+				firstBit ^= key[key.Length - polynomialDegrees[i]];
+			}
+
+			return firstBit;
+		}
+
+		private void ShiftToLeft(byte[] key)
+		{
+			for (int i = 0; i < key.Length - 1; i++)
+			{
+				key[i] = key[i + 1];
+			}
+		}
+
+		private void GenerateKey(int length)
+		{
+			keyBits = new byte[length];
+
+			for (int i = 0; i < length; i++)
+			{
+				keyBits[i] = register[0];
+
+				byte firstBit = GetFirstBit(register);
+				ShiftToLeft(register);
+				register[REGISTER_LENGTH - 1] = firstBit;
+			}
+		}
+
+		public void Encode()
+		{
+			resultBits = new byte[initialBits.Length];
+
+			GenerateKey(resultBits.Length);
+
+			for (int i = 0; i < resultBits.Length; i++)
+			{
+				resultBits[i] = (byte)(keyBits[i] ^ initialBits[i]);
+			}
+		}
+	}
+}
---
+++ Lab2_IT/Services/StreamManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_IT.Services
+{
+	public class StreamManager
+	{
+		internal static int BYTE_LENGTH = 8;
+
+		public byte[] BytesToBits(byte[] bytes)
+		{
+			byte[] bits = new byte[bytes.Length * BYTE_LENGTH];
+
+			for (int i = 0; i < bytes.Length; i++)
+			{
+				for (int j = 0; j < BYTE_LENGTH; j++)
+				{
+					bits[i * BYTE_LENGTH + j] = (byte)((bytes[i] >> j) & 0x1);
+				}
+			}
+
+			return bits;
+		}
+
+		public byte[] BitsToBytes(byte[] bits)
+		{
+			byte[] bytes = new byte[bits.Length / 8];
+
+			for (int i = 0; i < bytes.Length; i++)
+			{
+				byte oneByte = 0;
+
+				for (int j = 0; j < BYTE_LENGTH; j++)
+				{
+					oneByte |= (byte)(bits[i * BYTE_LENGTH + j] << j);
+				}
+
+				bytes[i] = oneByte;
+			}
+
+			return bytes;
+		}
+
+		public string BitsToString(byte[] bits)
+		{
+			StringBuilder bitOutput = new StringBuilder();
+
+			for (int i = 0; i < bits.Length; i++)
+			{
+				bitOutput.Append((bits[i] == 0) ? '0' : '1');
+				
+				if (i % BYTE_LENGTH == BYTE_LENGTH - 1)
+					bitOutput.Append(" ");
+			}
+			
+			return bitOutput.ToString().Trim();
+		}
+
+	}
+}
